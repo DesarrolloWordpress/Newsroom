051e68ded5e7f7feff6945cec4ab7e32e33353ce
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-07-09 13:55:56 -0500

    Esta todo roto!

--- app/User.php
+++ app/User.php
@@ -1,14 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
     use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -16,8 +24,27 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'liked_posts',
+        'disliked_posts',
+        'favourite_posts',
+        'favourite_categories',
+        'preferences',
+        'remember_token',
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'author_id', 'id');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'author_id', 'id');
+    }
 
     /**
      * The attributes that should be hidden for arrays.
@@ -25,7 +52,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -35,5 +65,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        \App\Models\User::factory(5)->create();
+        \App\Models\Category::factory(7)->create();
+        \App\Models\Post::factory(35)->create();
+
+        // post_tag table has no Model so to seed it we need our own logic
+        for ($i = 0; $i < 1500; $i++) {
+            DB::table('post_tag')->insertOrIgnore([
+                'post_id' => mt_rand(1, 35),
+                'tag_id' => mt_rand(1, 5),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
