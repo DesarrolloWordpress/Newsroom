d15ff7e98dacb1a48ea1adf1a72afe4f048fa8a1
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-08-11 17:57:46 -0500

    Controladores de Home, Post

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class HomeController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $posts = Post::where('published', 1)->orderBy('id', 'desc')->paginate(9);
+
+        return view('welcome', ['posts' => $posts]);
+        //return view('welcome');
+    }
+
+    /**
+     * Show the post identified by the $id
+     */
+    public function show($id)
+    {
+        //Find the post with the id = $id
+        $post = Post::find($id);
+        $posts = Post::where('published', 1)->orderBy('id', 'desc')->paginate(3);
+
+        return view('/entradas/show', compact('post', 'posts'));
+    }
+}
