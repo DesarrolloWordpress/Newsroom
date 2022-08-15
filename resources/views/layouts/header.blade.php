048973eaedf0da1eda2a9c755f81fba596fbf711
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-08-15 18:01:41 -0500

    Actualizadas Vistas

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Styles -->
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     {{-- <link rel="stylesheet" href="{{ mix('css/app.css', 'app') }}"> --}}
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/dibujables.css') }}" rel="stylesheet">
     <!-- font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
@@ -64,7 +67,8 @@
 
 <body>
     <header class="fixed-top">
-        <div class="container-md">
+        @livewire('navigation');
+        {{-- <div class="container-md">
             <div class="menu_head row flex-wrap">
                 <!-- Navigation -->
                 <div class="col-auto me-auto navbar align-items-end">
@@ -73,7 +77,7 @@
                             <ul class="navbar-nav">
                                 <li class="nav-item active">
                                     <a class="nav-link" href="#"> Inicio <span
-                                            class="sr-only">{{-- (current) --}}</span></a>
+                                            class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="https://reyma.com.mx/" target="_blank">Reyma</a>
@@ -94,5 +98,5 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,59 +2,81 @@
 @section('styles')
     <link href="css/home.css" rel="stylesheet">
     <style>
+        header .list_ico_share_f a {
+            color: #FFF;
+        }
+
+        .menu_head .nav-item a {
+            color: #fff;
+            padding: 0.35rem 0.9rem !important;
+        }
+
+        .menu_head .nav-item.active {
+            font-weight: 700;
+            background-color: #539bfd;
+        }
     </style>
 @endsection
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script></script>
+    <script>
+        (function($) {
+            $(document).ready(function() {
+                headersolid();
+                window.onscroll = function() {
+                    headersolid()
+                };
+
+                function headersolid() {
+                    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+                        $('header').addClass("active");
+                    } else {
+                        $('header').removeClass("active");
+                    }
+                }
+            });
+        })(jQuery);
+    </script>
 @endsection
-@section('content')
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <div class="panel-title">Sign In</div>
-                <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a>
+@section('banner-fondo-inicio')
+    <div class="px-3"
+        style="background-image: URL('{{ asset('images/home/instalaciones_banderas.jpg') }}'); background-size: cover; background-repeat: no-repeat;">
+        <div class="banner_head_home w-100">
+            <div class="container-md" style="color: #FFF">
+                <div class="col-auto d-flex justify-content-center">
+                    <div class="col-auto cont_img_b d-flex justify-content-end">
+                        <img src="{{ asset('images/home/reyma_logo.png') }}" style="max-width: 230px" />
+                    </div>
+                    <div class="mx-4" style="border-left:1px solid #FFF;"></div>
+                    <h2 class="col-auto text-left titulo_head_home my-4"> Newsroom </h2>
+                </div>
+                <div class="row justify-content-center my-3 pt-5">
+                    <h5 class="text-center mb-4" style="font-size: 2.3rem;font-weight: 700;">Bienvenido</h5>
+                    <p class="txt_font text-center col-md-6">Recibe antes que nadie las noticias relaciondas con los
+                        produtos <span class="txt_bold">REYMA</span>.</p>
+                </div>
+                <div class="input-group rounded pt-5 justify-content-center buscador_newsroom">
+                    <input type="search" class="form-control rounded bg-transparent" placeholder="Buscar"
+                        aria-label="Search" aria-describedby="search-addon" style="max-width: 320px" />
+                    <span class="input-group-text" id="search-addon">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </span>
                 </div>
             </div>
-            <div style="padding-top:30px" class="panel-body">
-                <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-                <form id="loginform" class="form-horizontal" role="form">
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-username" type="text" class="form-control" name="username" value=""
-                            placeholder="username or email">
-                    </div>
-                    <div style="margin-bottom: 25px" class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password"
-                            placeholder="password">
-                    </div>
-                    <div class="input-group">
-                        <div class="checkbox">
-                            <label>
-                                <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
-                            </label>
-                        </div>
-                    </div>
-                    <div style="margin-top:10px" class="form-group">
-                        <!-- Button -->
-                        <div class="col-sm-12 controls">
-                            <a id="btn-login" href="#" class="btn btn-success">Login </a>
-                            <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12 control">
-                            <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
-                                Don't have an account!
-                                <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-                                    Sign Up Here
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
         </div>
+    </div>
+@endsection
+@section('content')
+    <div class="container-md">
+        <div class="row my-5 pb-5">
+            {{-- @include('entradas.entrada') --}}
+        </div>
+        <div>
+            @include('mailing.formulario_suscripcion')
+        </div>
+        <div class="my-5 py-5">
+            @include('informacion')
+        </div>
+
     </div>
 @endsection
