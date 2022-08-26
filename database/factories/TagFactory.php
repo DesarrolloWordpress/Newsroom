e3c7e2233505ad1b50bea5b1db6767f7626a6ecf
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-08-26 13:55:11 -0500

    Agregado campo de color a etiqueta

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -8,9 +8,10 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(Tag::class, function (Faker $faker) {
-    $name = $this->faker->unique()->word(20);
+    $name = $this->faker->unique()->word('20');
     return [
         'name' => $name,
-        'slug' => Str::slug($name)
+        'slug' => Str::slug($name),
+        'color' => $this->faker->safeHexColor()
     ];
 });
