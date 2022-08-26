ba722a7ee5a5d75e378ac0e3e56fad9139934f54
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-08-26 13:55:59 -0500

    Ahora se agregan imagenes predescargadas de manera random

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -8,6 +8,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Image::class, function (Faker $faker) {
     return [
-        'url' => $this->faker->image('public/storage/posts', 640, 480, null, false),
+        /*$img = file_get_contents($this->faker->imageURL('640', '480')),
+        file_put_contents(storage_path() . '\app\public\posts', $img), */
+
+        // 'url' => $this->faker->image(storage_path() . '\app\public\posts', 640, 480, null, false),
+        'url' => 'posts/' . $this->faker->randomElement(['(1).jpg', '(2).jpg', '(3).jpg', '(4).jpg', '(5).jpg', '(6).jpg', '(7).jpg', '(8).jpg', '(9).jpg', '(10).jpg', '(11).jpg', '(12).jpg', '(13).jpg', '(14).jpg', '(15).jpg', '(16).jpg', '(17).jpg', '(18).jpg', '(19).jpg', '(20).jpg', '(21).jpg', '(22).jpg', '(23).jpg', '(24).jpg', '(25).jpg', '(26).jpg', '(27).jpg', '(28).jpg', '(29).jpg', '(30).jpg']),
     ];
 });
