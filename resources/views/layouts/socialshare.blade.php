8dc1e49d602f56a564107279526c3f4180859228
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-08-30 17:52:57 -0500

    Ligas de redes agregadas a rutas externas - Archivo

--- resources/views/layouts/socialshare.blade.php
+++ resources/views/layouts/socialshare.blade.php
@@ -1,37 +1,38 @@
 <ul class="socialM list-group list-group-horizontal d-flex justify-content-center">
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="Instagram" href="https://www.facebook.com/GrupoReymaOficial/">
+        <a class="social-icon" data-tooltip="Instagram" target="_blank" href="{{ route('external.red.facebook.reyma') }}">
             <i class="fa-brands fa-facebook-square"></i>
         </a>
     </li>
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="Instagram" href="https://www.instagram.com/reymaoficial/">
+        <a class="social-icon" data-tooltip="Instagram" target="_blank"
+            href="{{ route('external.red.instagram.reyma') }}">
             <i class="fa-brands fa-instagram"></i>
         </a>
     </li>
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="Twitter" href="https://twitter.com/gruporeyma/">
+        <a class="social-icon" data-tooltip="Twitter" target="_blank" href="{{ route('external.red.twitter.reyma') }}">
             <i class="fa-brands fa-twitter"></i>
         </a>
     </li>
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="YouTube" href="https://www.youtube.com/channel/UC9yoe6RVAkJEBMxJ4WTglCw/">
+        <a class="social-icon" data-tooltip="YouTube" target="_blank" href="{{ route('external.red.youtube.reyma') }}">
             <i class="fa-brands fa-youtube"></i>
         </a>
     </li>
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="Tiktok" href="https://www.tiktok.com/@reymaoficial?lang=en/">
+        <a class="social-icon" data-tooltip="Tiktok" target="_blank" href="{{ route('external.red.tiktok.reyma') }}">
             <i class="fa-brands fa-tiktok"></i>
         </a>
     </li>
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="LinkedIn" href="https://www.linkedin.com/company/grupo-reyma/">
+        <a class="social-icon" data-tooltip="LinkedIn" target="_blank"
+            href="{{ route('external.red.linkedin.reyma') }}">
             <i class="fa-brands fa-linkedin-in"></i>
         </a>
     </li>
     <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="Spotify"
-            href="https://open.spotify.com/user/31n5gyqzjyqioodddcsjdzkgqkoa?si=oxIbR1xPRnWL6ikEH91ZPw&nd=1">
+        <a class="social-icon" data-tooltip="Spotify" target="_blank" href="{{ route('external.red.spotify.reyma') }}">
             <i class="fa-brands fa-spotify"></i>
         </a>
     </li>
