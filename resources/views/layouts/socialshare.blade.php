469d491e161823e35521d0cad894afbc34ac6abc
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-09-07 17:55:45 -0500

    Social Share actualizado para tailwind

--- resources/views/layouts/socialshare.blade.php
+++ resources/views/layouts/socialshare.blade.php
@@ -1,37 +1,44 @@
-<ul class="socialM list-group list-group-horizontal d-flex justify-content-center">
-    <li class="list_ico_share_f d-flex align-items-center">
-        <a class="social-icon" data-tooltip="Instagram" target="_blank" href="{{ route('external.red.facebook.reyma') }}">
+<ul class="ml-auto flex items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block border-transparent border-2 hover:border-blue-400">
+        <a class="social-icon" data-tooltip="Facebook" target="_blank" href="{{ route('external.red.facebook.reyma') }}">
             <i class="fa-brands fa-facebook-square"></i>
         </a>
     </li>
-    <li class="list_ico_share_f d-flex align-items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block ml-1 border-transparent border-2 hover:border-blue-400">
         <a class="social-icon" data-tooltip="Instagram" target="_blank"
             href="{{ route('external.red.instagram.reyma') }}">
             <i class="fa-brands fa-instagram"></i>
         </a>
     </li>
-    <li class="list_ico_share_f d-flex align-items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block ml-1 border-transparent border-2 hover:border-blue-400">
         <a class="social-icon" data-tooltip="Twitter" target="_blank" href="{{ route('external.red.twitter.reyma') }}">
             <i class="fa-brands fa-twitter"></i>
         </a>
     </li>
-    <li class="list_ico_share_f d-flex align-items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block ml-1 border-transparent border-2 hover:border-blue-400">
         <a class="social-icon" data-tooltip="YouTube" target="_blank" href="{{ route('external.red.youtube.reyma') }}">
             <i class="fa-brands fa-youtube"></i>
         </a>
     </li>
-    <li class="list_ico_share_f d-flex align-items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block ml-1 border-transparent border-2 hover:border-blue-400">
         <a class="social-icon" data-tooltip="Tiktok" target="_blank" href="{{ route('external.red.tiktok.reyma') }}">
             <i class="fa-brands fa-tiktok"></i>
         </a>
     </li>
-    <li class="list_ico_share_f d-flex align-items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block ml-1 border-transparent border-2 hover:border-blue-400">
         <a class="social-icon" data-tooltip="LinkedIn" target="_blank"
             href="{{ route('external.red.linkedin.reyma') }}">
             <i class="fa-brands fa-linkedin-in"></i>
         </a>
     </li>
-    <li class="list_ico_share_f d-flex align-items-center">
+    <li
+        class="list_ico_share_f min-w-[30px] flex justify-center items-center aspect-square rounded-full inline-block ml-1 border-transparent border-2 hover:border-blue-400">
         <a class="social-icon" data-tooltip="Spotify" target="_blank" href="{{ route('external.red.spotify.reyma') }}">
             <i class="fa-brands fa-spotify"></i>
         </a>
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,5 +1,5 @@
-<nav x-data="{ open: false }" class="bg-white-800">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+<nav x-data="{ open: false }" class="bg-white-800 pt-3">
+    <div class="max-w-screen-2xl mx-auto px-2 sm:px-6 lg:px-1">
         <div class="relative flex items-center justify-between h-16">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <!-- Mobile menu button-->
@@ -7,24 +7,12 @@
                     class="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                     aria-controls="mobile-menu" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
-                    <!--
-              Icon when menu is closed.
-  
-              Heroicon name: outline/menu
-  
-              Menu open: "hidden", Menu closed: "block"
-            -->
+                    <!-- Icon when menu is closed. Heroicon name: outline/menu Menu open: "hidden", Menu closed: "block" -->
                     <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
-                    <!--
-              Icon when menu is open.
-  
-              Heroicon name: outline/x
-  
-              Menu open: "block", Menu closed: "hidden"
-            -->
+                    <!-- Icon when menu is open. Heroicon name: outline/x Menu open: "block", Menu closed: "hidden" -->
                     <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -38,11 +26,11 @@
                     <img class="hidden lg:block h-8 w-auto"
                         src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500" alt="Workflow"> --}}
                 </div>
-                <div class="menu_head hidden sm:block sm:ml-6">
-                    <div class="flex space-x-4">
+                <div class="menu_head hidden sm:block sm:ml-3">
+                    <div class="flex items-center h-full space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="{{ route('home') }}"
-                            class="active text-white px-3 py-2 rounded-md text-sm font-semibold"
+                            class="active bg-[#539bfd] text-white px-3 py-2 text-sm font-semibold active:bg-amber-500"
                             aria-current="page">Inicio</a>
 
                         <a href="{{ route('external.reyma.home') }}"
