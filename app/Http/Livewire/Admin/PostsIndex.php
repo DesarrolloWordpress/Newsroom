ebca9d9ba553a55a5c999f8bbd7a3c680890c9a9
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-09-08 17:50:42 -0500

    Aplicando estilos tailwind a paginador

--- app/Http/Livewire/Admin/PostsIndex.php
+++ app/Http/Livewire/Admin/PostsIndex.php
@@ -11,7 +11,7 @@ class PostsIndex extends Component
 
     use WithPagination;
 
-    protected $paginationTheme = "bootstrap";
+    // protected $paginationTheme = "tailwind";
     public $search;
 
     public function updatingSearh()
