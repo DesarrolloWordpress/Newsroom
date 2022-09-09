592dd7c3ed2448112de12c863b175eb377166a88
DanielWuenoE <daniel_bueno.be@hotmail.com>
2022-09-09 15:25:47 -0500

    Uso de postCard como tema para generar la lista

--- resources/views/posts/entrada.blade.php
+++ resources/views/posts/entrada.blade.php
@@ -1,51 +1,5 @@
 @foreach ($posts as $post)
     <div class="my-[2em]">
-        <div class="max-h-[270px] h-full">
-            <img class="mt-5 object-cover h-full w-full"
-                src="@if ($post->image) {{ Storage::url($post->image->url) }}
-                @else
-                {{ asset('images/default/img_default.jpg') }} @endif
-                "
-                alt="post_image">
-        </div>
-        <div class="">
-            <div class="my-3 flex">
-                <div class="text-sm text-start w-1/2">
-                    {{-- @foreach ($post->category as $category) --}}
-                    <label
-                        class="font-bold @if ($post->category) text-[{{ $post->category->color }}] @else text--[ncategory] @endif"
-                        for="">
-                        @if ($post->category)
-                            {{ $post->category->name }}
-                        @endif
-                    </label>
-                    @foreach ($post->tags as $tag)
-                        {{-- <label class="" for=""
-                            style="font-weight: 600;font-size: 16px;">{{ $tag->name }}
-                        </label> --}}
-                    @endforeach
-                    {{-- @endforeach --}}
-                </div>
-                <div class="text-sm text-end w-1/2">
-                    <label class="w-full" for="">Jun 01
-                        2022</label>
-                </div>
-            </div>
-            <hr class="max-w-[50px] border-0 border-t-2 border-[#b8b8b8]">
-            <div class="">
-                <a href="{{ route('post.show', $post) }}">
-                    <div class="h-[4rem] font-bold my-4 text-2xl hover:text-[#569bfa]">
-                        <h3>
-                            {{ $post['name'] }}
-                        </h3>
-                    </div>
-                </a>
-                <div class="text-lg">
-                    <p>
-                        {!! substr($post['extract'], 0, 100) !!}
-                    </p>
-                </div>
-            </div>
-        </div>
+        <x-postcard postid='{{ $post->id }}'></x-postcard>
     </div>
 @endforeach
--- resources/views/posts/entradaRelacionada.blade.php
+++ resources/views/posts/entradaRelacionada.blade.php
@@ -1,35 +1,10 @@
-@foreach ($relacionados as $relacionado)
-    <div class="col-xl-4 col-md-6">
-        <div class="max-h-[270px] h-full">
-            {{-- <img class="img-fluid" src="http://placeimg.com/450/270" alt="img_pruebas"> --}}
-            <img class="img-thumbnail object-cover mt-5 h-full w-full" src="{{ Storage::url($relacionado->image->url) }}"
-                alt="post_image">
+<div class="w-full">
+    <h5 class="text-center text-3xl font-bold">NOTAS RELACIONADAS</h5>
+</div>
+<div class="mb-20 mt-5 grid grid-cols-3 gap-5 max-w-screen-xl">
+    @foreach ($relacionados as $relacionado)
+        <div>
+            <x-postcard postid='{{ $relacionado->id }}'></x-postcard>
         </div>
-        <div class="">
-            <div class="row mt-3">
-                <div class="post_category col-md-6">
-                    <label class="" for="" style="font-weight: 600;font-size: 16px;">Specialist</label>
-                </div>
-                <div class="post_date col-md-6">
-                    <label class="w-100 m-0" style="text-align: right;font-size: 16px;" for="">Jun 01
-                        2022</label>
-                </div>
-            </div>
-            <hr style="max-width: 50px;height: 2px;">
-            <div class="">
-                <a href="{{ $relacionado['id'] }}">
-                    <div class="post_title">
-                        <h3 style="font-weight: 600;">
-                            {{ $relacionado['name'] }}
-                        </h3>
-                    </div>
-                </a>
-                <div class="post_content_ext">
-                    <p>
-                        {!! substr($relacionado['content'], 0, 100) !!}
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-@endforeach
+    @endforeach
+</div>
