858a5a3d34886c077e6616b27787e1f27f8ed1a4
Daniel B <50934699+DanielWuenoE@users.noreply.github.com>
2023-02-03 15:19:47 -0600

    Delete script SQL

--- pruebasSQL.sql
+++
@@ -1,9 +0,0 @@
-USE newsroom;
-
-SELECT * FROM users;
-SELECT * FROM tags;
-SELECT * FROM permissions;
-SELECT * FROM categories;
-SELECT * FROM posts;
-SELECT * FROM images;
-SELECT * FROM post_tag;languaje
